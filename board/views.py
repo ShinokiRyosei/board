2d6bf20ffa8034d8296b2040bf426805c61f4106
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-05-29 10:07:04 +0900

    [Add] Django REST Frameworks

--- board/views.py
+++ board/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.template import loader, RequestContext
+from board.forms import PostForm, SignupForm, LoginForm
 # Create your views here.
 
 def  index(request):
@@ -10,5 +11,5 @@ def  index(request):
 def signup(request):
     user = User()
     template = loader.get_template('signup.html')
-    form =
+    form = SignupForm
     return render_to_response(template, {'form': form}, context_instance=RequestContext(request))
